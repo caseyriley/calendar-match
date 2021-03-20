5bb2a4de553e778d4ef5346b60fa9fa10376c94e
Casey <innerforest7@gmail.com>
2021-03-19 21:12:47 -0700

    setup name inputs

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,18 +1,24 @@
 <template>
   <h1>Find all available appointment times</h1>
-  <label>
+  <label >
     What is person one's name?
-    <input />
-  </label>
-  <label>
+    <input v-model="name1" />
+  </label >
+  <label >
     What is person two's name?
-    <input />
+    <input v-model="name2" />
   </label>
 </template>
 
 <script>
 export default {
   name: "LandingPage",
+  data() {
+    return {
+      name1: null,
+      name2: null,
+    };
+  },
 };
 </script>
 
