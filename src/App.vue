5c3e919a5c160a9c07907583431fb401bfde8d71
Casey <innerforest7@gmail.com>
2021-03-19 20:27:44 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,15 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100vw;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  background: cornflowerblue;
 }
 
 #nav {
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Find all available appointment times</h1>
+  <label>
+    What is person one's name?
+    <input />
+  </label>
+  <label>
+    What is person two's name?
+    <input />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
